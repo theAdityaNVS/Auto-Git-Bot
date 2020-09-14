df0db398083c427f45c95120b6896d6da44d45a3
theAdityaNVS <aditya.nadamuni@gmail.com>
2020-09-14 11:28:35 -0700

    script completed

---
+++ watchBot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+path="/home/aditya/Desktop/Auto-Git-Bot" 
+echo "---AutoGitBot Active---"
+echo "Now Monitoring for Changes ...."
+while true 
+do
+	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
+	cd $path
+	git add --all
+	checkin=$(date)
+    echo "New Changes Detected @ $checkin"
+	git commit -m "AutoGitBot: Changes Added @ $checkin"
+	git pull origin master
+	git push -u origin master
+    echo "Changes are Updated with Remote"
+    echo "Now Monitoring for Changes .... (Ctrl + Z to Exit)"
+done
